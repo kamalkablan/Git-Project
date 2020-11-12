6e803a2ee5c5404221230d8bbdf749d7b213a26c
kamalkablan <kimokablan10@gmail.com>
2020-11-12 10:37:54 -0500

    Initial commit

---
+++ src/FirstClass.java
@@ -0,0 +1,12 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("let's do this thing");
+        int number1 =10;
+        int number2 = 19;
+        if (number1 == number2){
+            System.out.println("They are equal");
+        }else{
+            System.out.println("They are not equal");
+        }
+    }
+}
